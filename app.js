0f9decaf913cc87324a3736ea12a42f175ecc9fc
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-03-11 11:44:58 +0530

    testing view js

--- app.js
+++ app.js
@@ -5,32 +5,28 @@ const express = require('express');
 
 const app = express();
 
+// register view Engine 
+
+app.set('view engine','ejs');
+
 // listen for request
 
 app.listen(3000);
 
+
 app.get('/',(req, res) => {
-    res.send('<p>Home Page</p>');
+    res.render('index');
 })
 
-app.get('/about-us', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
-})
-
-app.get('/contact', (req, res) => {
-    res.sendFile('./views/blog.html', {root: __dirname}); 
+app.get('/about', (req, res) => {
+    res.render('about');
 })
 
 
-// redirects
 
-app.get('/about-me', (req, res) => {
-    res.redirect('/about-us');         // to work wit h redirects you ,ust declared the function like above when you write if user request about-us in to url tab and then respond with the file directory of html
-                                        // after that if your mistakenly go to the similiar but wrong name like about-me instead of about-us you can redirect it with this function besides this comment    
-})
 
 // 404 pages
 
 app.use((req, res) => {
-    res.sendFile('./views/blog.html', {root: __dirname});
+    res.status(404).render('404');
 })
