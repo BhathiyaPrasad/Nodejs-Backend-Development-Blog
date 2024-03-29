1934e0c362119adb9b585d9baeb9ca2462999f3c
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-03-29 20:29:12 +0530

    update

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { method, result } = require('lodash');
 const morgan = require('morgan');
 const mongoose = require('mongoose'); 
-const Blog = require('./models/blog');
+
 const { render } = require('ejs');
 const { request } = require('http');
+const blogRoutes = require('./routes/blogRouter');
 
 // express app
 const app = express();
@@ -80,55 +81,7 @@ app.get('/about', (req, res) => {
 
 // blog routes
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
-app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then(result => {
-      res.render('index', { blogs: result, title: 'All blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-app.post('/blogs', (req, res) =>{
-  console.log(req.body);
-  const blog = new Blog(req.body);
-
-  blog.save()
-  .then((result) =>{
-    res.redirect('/blogs');
-  })
-  .catch((err) => {
-     console.log(err);
-  })
-})
-app.get('/blogs/:id', (req, res) => {
-  const id = req.params.id;
- 
-  Blog.findById(id)
-  .then(result => {
-    res.render('details', { blog:result, title:'Blog Details'})
-  })
-  .catch(err => {
-    console.log(err);
-  });
-})
-
-// This will find the id in database and delete the record
-
-app.delete('/blogs/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-  .then(result => {
-     res.json({redirect: '/blogs'})
-  })
-  .catch(err => {
-    console.log(err)
-  })
-})
+app.use('/blogs', blogRoutes);
 
 
 
---
+++ routes/blogRouter.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const Blog = require('../models/blog');
+const router = express.Router();
+
+
+
+router.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
+  });
+  
+  router.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+      .then(result => {
+        res.render('index', { blogs: result, title: 'All blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  router.post('/blogs', (req, res) =>{
+    console.log(req.body);
+    const blog = new Blog(req.body);
+  
+    blog.save()
+    .then((result) =>{
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+       console.log(err);
+    })
+  })
+  router.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+   
+    Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog:result, title:'Blog Details'})
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  })
+  
+  // This will find the id in database and delete the record
+  
+  router.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+    .then(result => {
+       res.json({redirect: '/blogs'})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  })
+
+  module.exports = router;
